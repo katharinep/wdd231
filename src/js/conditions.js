21b3f938fbb20727c3a049f1fad75c77cb0b6315
Katharine <katharine.risingphoenix@gmail.com>
2025-10-23 18:08:08 -0600

    Progress through to step 4.

--- src/js/conditions.js
+++ src/js/conditions.js
@@ -1,9 +1,30 @@
-import { getParkData } from "./parkService.mjs";
+import "../css/style.css";
+import "../css/conditions.css";
+
+import { getParkData, getParkAlerts } from "./parkService.mjs";
+import { alertTemplate } from "./templates.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
+
+function setAlerts(alerts) {
+  const alertsContainer = document.querySelector(".alerts > ul");
+  alertsContainer.innerHTML = "";
+  const html = alerts.map(alertTemplate);
+  alertsContainer.insertAdjacentHTML("beforeend", html.join(""));
+}
 
 async function init() {
   const parkData = await getParkData();
+  const alerts = await getParkAlerts(parkData.parkCode);
   setHeaderFooter(parkData);
+  setAlerts(alerts);
+}
+
+function getVisitorCenterData(parkCode) {
+
+}
+
+function setVisitorsCenters() {
+    
 }
 
 init();
